8be88a876d26a0ec6a71cd2efd6393b84d4f41b9
daeho-haruto <kimdaeho2004@naver.com>
2025-09-16 14:22:53 +0900

    Fiexed: vscode가 .clang-foramt을 못 따라가는 오류 해결

--- src/utils/timer.hpp
+++ src/utils/timer.hpp
@@ -2,6 +2,7 @@
 #define TIMER_HPP
 
 #include <chrono>
+#include <ctime>
 #include <iostream>
 #include <string>
 #include <utility>
@@ -11,16 +12,13 @@ namespace std_cpp::utils {
 class Timer {
  public:
   Timer(std::string name = "")
-      : name(std::move(name)),
-        start_time(std::chrono::high_resolution_clock::now()) {}
+      : name(std::move(name)), start_time(std::chrono::high_resolution_clock::now()) {}
 
   ~Timer() {
     auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
-                        end_time - start_time)
-                        .count();
-    std::cout << "[TIMER] " << name << " 실행 시간: " << duration
-              << " 마이크로초\n";
+    auto duration =
+        std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    std::cout << "[TIMER] " << name << " 실행 시간: " << duration << " 마이크로초\n";
   }
 
   Timer(const Timer&) = delete;
